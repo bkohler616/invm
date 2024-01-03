5e5859e1a6e68cbd9a8db2df857a11f2ffe2b69a
bkohler616 <bkohler616@hotmail.com>
2024-01-03 12:51:11 -0500

    feature: add production tsconfig, add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('testy');
+import yargs, {Options} from 'yargs';
+
+const testOptions: { [key: string]: Options; } = {
+    p: {
+        type: "string",
+        default: false
+    },
+    ppp: {
+        type: "string",
+        default: false
+    }
+};
+
+const argv = yargs(process.argv.slice(2)).options(testOptions).parseSync();
+console.log('(%d,%d)', argv.p, argv.ppp);
+
+console.log(JSON.stringify(argv));
